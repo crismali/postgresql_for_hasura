d220e071c6c5cd805e169e08eb0b7e3dd67736e6
Michael Crismali <michael.crismali@focusedlabs.io>
2022-08-27 16:01:44 -0500

    More output adjustments

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,11 @@ output "postgresql_for_hasura_database_endpoint" {
 output "postgresql_for_hasura_database_url" {
   description = "The complete database URL you can paste into Hasura. `terraform output -raw postgresql_for_hasura_database_url | pbcopy`"
   sensitive   = true
-  value       = "postgresql://${var.database_username}:${var.database_password}@${module.aws_rds_cluster.cluster.endpoint}:5432/${var.database_name}"
+  value       = "postgresql://${var.database_username}:${var.database_password}@${aws_rds_cluster.cluster.endpoint}:5432/${var.database_name}"
 }
 
 output "postgresql_for_hasura_passwordless_database_url" {
   description = "A valid database URL with 'REPLACE_WITH_PASSWORD' instead of your database's password."
   sensitive   = false
-  value       = "postgresql://${var.database_username}:REPLACE_WITH_PASSWORD@${module.aws_rds_cluster.cluster.endpoint}:5432/${var.database_name}"
+  value       = "postgresql://${var.database_username}:REPLACE_WITH_PASSWORD@${aws_rds_cluster.cluster.endpoint}:5432/${var.database_name}"
 }
